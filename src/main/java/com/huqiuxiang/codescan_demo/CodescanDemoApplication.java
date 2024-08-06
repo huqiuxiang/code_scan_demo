7b756de7f29d8940d9225ad87cb8d7419bdda809
huqiuxiang <hqxgaoq159@163.com>
2024-08-06 15:46:40 +0800

    fist commit

---
+++ src/main/java/com/huqiuxiang/codescan_demo/CodescanDemoApplication.java
@@ -0,0 +1,13 @@
+package com.huqiuxiang.codescan_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodescanDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodescanDemoApplication.class, args);
+    }
+
+}
