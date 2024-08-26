dda56ea274cc8dac1f07207555b1e82c1dbf84c2
huqiuxiang <71514213+huqiuxiang@users.noreply.github.com>
2024-08-26 16:55:15 +0800

    Update TestController.java

--- src/main/java/com/huqiuxiang/codescan_demo/controller/TestController.java
+++ src/main/java/com/huqiuxiang/codescan_demo/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/test2")
+    @GetMapping("/test3")
     public String test(){
         return "hello test!xxxx hahaha ";
     }
